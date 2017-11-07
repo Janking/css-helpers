41da068fa96fcf29b777e06b8cb57794d6836cd3
Janking <315359131@qq.com>
2017-11-07 02:22:18 -0600

    Update and rename intercept.js to not-intercept.js

---
+++ not-intercept.js
@@ -0,0 +1,2 @@
+let img = new Image()
+img.src = 'http://ia.51.la/go.asp?svid=15&id=19236546&tpages=4&ttimes=4&tzone=8&tcolor=24&sSize=1920,1080&referrer=&vpage=http%3A//192.168.0.75%3A3000/&vvtime=1510041654399'
